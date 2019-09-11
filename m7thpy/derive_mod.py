77bb142e2867bc35c973e2df2fdd2d8766748a90
omegxis <39287514+omegxis@users.noreply.github.com>
2019-09-11 15:30:17 -0400

    Add files via upload

--- m7thpy/derive_mod.py
+++ m7thpy/derive_mod.py
@@ -4,69 +4,78 @@ from sympy import diff
 from sympy.parsing.sympy_parser import parse_expr
 
 def derive_fun(input_val):
-	evaluate_at = True
-	x = None
-	der = None
-	gr = None
-	point = None
-
-	try:
-		int(input_val[len(input_val)-1])
-	except ValueError:
-		evaluate_at = False
-
-
-	#not the most efficient method for determining what the user wants. Find another way to make it more efficient
-	#Maybe use switch cases
-
-	#to evaluate a derivative at point: derivative_variable.evalf(subs={x: value1, y: value2, ...})
-
-	#graph and evaluate
-	if (input_val[1] == "graph" and evaluate_at == True):
-
-		point = int(input_val[4])
-		x = symbols(input_val[3])
-
-		der = diff(input_val[2], x)
-		pder = der.evalf(subs={x: point})
-
-		print(pder)
-		gr = plot(der, show=False)
-		gr.show()
-		gr.close()
-
-	#graph and do not evaluate
-	elif(input_val[1] == "graph" and evaluate_at == False):
-
-		x = symbols(input_val[3])
-		der = diff(input_val[2], x)
-		print(der)
-		gr = plot(der, show=False)
-		gr.show()
-
-	#do not graph and evaluate
-	elif (input_val[1] != "graph" and evaluate_at == True):
-		point = int(input_val[3])
-		x = symbols(input_val[2])
-
-		der = diff(input_val[1], x)
-		pder = der.evalf(subs={x:point})
-
-		print(pder)
+        #Evaluates single variable derivative
+        #Non-polar, parametric.
+        #Add more documentation and clean up code. 
+        evaluate_at = True
+        x = None
+        der = None
+        gr = None
+        point = None
+        is_frac = True
 
 
-	#do not graph and do not evaluate
-	elif (input_val[1] != "graph" and evaluate_at == False):
-		x = symbols(input_val[2])
-		der = diff(input_val[1], x)
-		print(der)
-	else: #This expression might not be needed, since regex takes care of problematic input. It doesn't hurt to have a fail safe
-		print("Error in expression. Type help(derivative) to read about input. Type example derivative to see an example")
+        fr_ar = input_val[len(input_val)-1].split("/")
+        
+        if (len(fr_ar) == 1):
+                #last input is only a single entity. We can check if its an int or anything else
+                try:
+                        int(input_val[len(input_val)-1])
+                        
+                except ValueError:
+                        evaluate_at = False
 
+        else:
+                #Last input must be a fraction
+                tn = input_val[len(input_val)-1]
+                input_val[len(input_val)-1] = eval(tn)
 
-		
+        #Not the most efficient method for determining what the user wants. Find another way to make it more efficient
+        #Maybe use switch cases
 
+        #to evaluate a derivative at point: derivative_variable.evalf(subs={x: value1, y: value2, ...})
 
+        #graph and evaluate
+        if (input_val[1] == "graph" and evaluate_at == True):
+                point = float(input_val[4])
+                x = symbols(input_val[3])
+
+                der = diff(input_val[2], x)
+                pder = der.evalf(subs={x: point})
+
+                print("Derivative: ", der)
+                print("Derivative evaluated at given value = ", pder)
+                gr = plot(der, show=False)
+                gr.show()
+               
+        #graph and do not evaluate
+        elif(input_val[1] == "graph" and evaluate_at == False):
+
+                x = symbols(input_val[3])
+                der = diff(input_val[2], x)
+                print("Derivative: ", der)
+                gr = plot(der, show=False)
+                gr.show()
+
+        #do not graph and evaluate
+        elif (input_val[1] != "graph" and evaluate_at == True):
+                point = float(input_val[3])
+                x = symbols(input_val[2])
+
+                der = diff(input_val[1], x)
+                pder = der.evalf(subs={x:point})
+
+                print("Derivative: ", der)
+                print("Derivative evaluated at given value = ", pder)
+
+        #do not graph and do not evaluate
+        elif (input_val[1] != "graph" and evaluate_at == False):
+                x = symbols(input_val[2])
+                der = diff(input_val[1], x)
+                print("Derivative: ", der)
+                
+        else: #This expression might not be needed, since regex takes care of problematic input. It doesn't hurt to have a fail safe
+                print("Error in expression. Type help(derivative) to read about input. Type example derivative to see an example")
 
 
 
--- m7thpy/m7th.py
+++ m7thpy/m7th.py
@@ -1,39 +1,40 @@
 import re
 from derive_mod import derive_fun
 
-
 def call_derivative(input_val):
-	temparray = input_val.split()
-	print(temparray)
-	derive_fun(temparray)
-	
+        temparray = input_val.split()
+        print(temparray)
+        derive_fun(temparray)
+        
 def call_integral():
-	print("test")
+        print("test")
 
 
 def main():
-	input_val = ""
-	quit_token = "-quit"
+        input_val = ""
+        quit_token = "-quit"
 
-	while input_val != quit_token:
-		first_message = "Welcome to m7thpy. Input a command, according the the key. Type help(command) for clarification "
-		print(first_message)
-		input_val = input()
+        while input_val != quit_token:
+                first_message = "Welcome to m7thpy. Input a command, according the the key. Type help(command) for clarification "
+                print(first_message)
+                input_val = input()
 
-		#Might assign "graph" to variable so it can be changed to anything to user may want, if I add that feature
-		#Note from September 10th: Allow regex to accept fractions and special constants
-		regex_rules = "^(derive)\s+((graph\s[^\s]+)|([^\s]+))\s+((([a-zA-Z])\s+([-+]?\d+(\.\d+)?){0,1})|([a-zA-Z]))\s*$"
-		match = re.search(regex_rules, input_val)
+                #Might assign "graph" to variable so it can be changed to anything to user may want, if I add that feature
+                #Note from September 10th: Allow regex to accept fractions and special constants
+                #regex_rules = "^(derive)\s+((graph\s[^\s]+)|([^\s]+))\s+((([a-zA-Z])\s+([-+]?\d+(\.\d+)?){0,1})|([a-zA-Z]))\s*$"
+                #Keep regex_rules as a backup
+                regex_rules = "^(derive)\s+((graph\s[^\s]+)|([^\s]+))\s+((([a-zA-Z])\s+([-+]?\d+(\.\d+)?){0,1})|[a-zA-Z]\s+[-+]{0,1}\d+\/[-+]{0,1}\d+|([a-zA-Z]))\s*$"
+                match = re.search(regex_rules, input_val)
 
-		#have seperate module for adding "*" when doing multiplication
-		#input problems with e. sympy requires exp(x) to understand e^x. Seperate module must be made for cleaning input	
-		if (match):
-			print("acceptable input")
-			call_derivative(input_val)
-		elif (input_val != quit_token):
-			print("unacceptable input")
-	print("Program closed")
+                #have seperate module for adding "*" when doing multiplication
+                #input problems with e. sympy requires exp(x) to understand e^x. Seperate module must be made for cleaning input        
+                if (match):
+                        print("acceptable input")
+                        call_derivative(input_val)
+                elif (input_val != quit_token):
+                        print("unacceptable input")
+        print("Program closed")
 
 
 if __name__ == '__main__':
-	main()
+        main()
